d4db88a5488153f46ded1598c38c3612f3f911d5
Mikhail Sinelnikov <msin87@yandex.ru>
2020-09-09 21:36:30 +0300

    added udp servers factory

--- udp_server.js
+++ udp_server.js
@@ -1,9 +1,44 @@
 const dgram = require('dgram');
 const udpServer = dgram.createSocket('udp4');
 
-udpServer.on('listening', ()=>console.log('UDP server listening on 443 port'))
-udpServer.on("error", (err)=>console.log(err));
-udpServer.on("message", (localReq, linfo)=>{
-    console.log(localReq, linfo);
-})
-udpServer.bind(443);
+const udpServerFactory = (address = [{
+    ip: '192.168.1.50', port: 443, msgCallback: (req, info) => {
+    }
+}]) => {
+    const udpServers = [];
+    address.forEach(record => {
+        const udpServer = dgram.createSocket('udp4');
+        udpServer.on('listening', () => console.log(`UDP server listening ip: ${record.ip}, port: ${record.port}`))
+        udpServer.on("error", (err) => console.log(err));
+        udpServer.on("message", record.msgCallback)
+        udpServers.push(udpServer)
+    })
+    return udpServers;
+}
+
+const udpLogger = (req, info) => {
+    console.log('req:' + req);
+    console.log('info: ' + info)
+}
+
+const udpServers = udpServerFactory([
+    {
+        ip: '192.168.1.50',
+        port: 443,
+        msgCallback: udpLogger
+    },
+    {
+        ip: '192.168.1.51',
+        port: 443,
+        msgCallback: udpLogger
+    },
+    {
+        ip: '192.168.1.52',
+        port: 443,
+        msgCallback: udpLogger
+    },{
+        ip: '192.168.1.67',
+        port: 443,
+        msgCallback: udpLogger
+    },
+])
